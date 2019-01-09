b988282553a41ff37e6032198a9a62af294738c0
Matt Button <matt.button@geckoboard.com>
2019-01-09 18:08:31 +0000

    Add modules for configuring AWS IAM users/roles for slash-infra

---
+++ terraform/aws-app-user/main.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_user" "app-user" {
+  name = "${var.username}"
+}
+
+resource "aws_iam_user_policy" "allow-assuming-slash-infra-roles" {
+  name = "allow-assuming-slash-infra-roles"
+  user = "${aws_iam_user.app-user.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "AllowAssumingSlashInfraRoles",
+          "Effect": "Allow",
+          "Action": "sts:AssumeRole",
+          "Resource": "arn:aws:iam::*:role/${var.role_name}"
+      }
+  ]
+}
+EOF
+}
